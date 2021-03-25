f3c007d3008b1f3a1d63c7dfb947b1a8ed356c72
moshe212 <moshe212@gmail.com>
2021-03-26 01:13:03 +0200

    update api_index

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,18 +5,4 @@ router.use("/orders", require("./orders"));
 router.use("/products", require("./products"));
 router.use("/login", require("./login"));
 
-router.use(function (err, req, res, next) {
-  if (err.name === "ValidationError") {
-    return res.status(422).json({
-      errors: Object.keys(err.errors).reduce(function (errors, key) {
-        errors[key] = err.errors[key].message;
-
-        return errors;
-      }, {}),
-    });
-  }
-
-  return next(err);
-});
-
 module.exports = router;
