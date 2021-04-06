ebc84d3e859f78a9ece78be7dc054df312608d63
moshe212 <moshe212@gmail.com>
2021-04-06 14:46:54 +0300

    fix mcv

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,24 @@
+// const router = require("express").Router();
+
+// router.use("/", require("./products"));
+// router.use("/orders", require("./orders"));
+// router.use("/products", require("./products"));
+// router.use("/login", require("./login"));
+
+// module.exports = router;
+
 const router = require("express").Router();
 
-router.use("/", require("./products"));
-router.use("/orders", require("./orders"));
-router.use("/products", require("./products"));
-router.use("/login", require("./login"));
+router
+  .get("/", function (req, res) {
+    res.json({
+      status: "API Its Working",
+      message: "Welcome to shop!",
+    });
+  })
+  .use("/", require("./products"))
+  .use("/orders", require("./orders"))
+  .use("/products", require("./products"))
+  .use("/login", require("./login"));
 
 module.exports = router;
